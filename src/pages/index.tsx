4894579d8c172ea616b33f59875a71d25ffa8f34
Folded-Code <thomas.reilly.forbes@gmail.com>
2022-11-13 13:58:19 +0000

    Got audio visualising working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
+// @ts-ignore
 import MicRecorder from 'mic-recorder-to-mp3'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
-import { FaHeart, FaMicrophone, FaRegHeart, FaStopCircle } from 'react-icons/fa'
+import {
+  FaHeart,
+  FaMicrophone,
+  FaPause,
+  FaPlay,
+  FaRegHeart,
+  FaStopCircle,
+  FaTrash,
+} from 'react-icons/fa'
 import TextareaAutosize from 'react-textarea-autosize'
 
-const Mp3Recorder = new MicRecorder({ bitRate: 128 })
 interface Comment {
   label: string
   rating: number
@@ -12,7 +20,6 @@ interface Comment {
 
 export default function Home() {
   // const hello = trpc.example.hello.useQuery({ text: 'from tRPC' })
-
   const podcast = {
     title:
       'Why you need a single focus (and ditch your portfolio of projects) - Chris Frantz, Loops',
@@ -56,26 +63,6 @@ export default function Home() {
     { label: 'Production', rating: -1 },
   ])
 
-  const [isRecording, setIsRecording] = useState(false)
-  const [recURL, setRecURL] = useState<string | null>(null)
-
-  const startRecording = async () => {
-    Mp3Recorder.start()
-      .then(() => {
-        setIsRecording(true)
-      })
-      .catch((e: Error) => console.error(e))
-  }
-  const stopRecording = async () => {
-    Mp3Recorder.stop()
-      .getMp3()
-      .then(([_, blob]: [any, Blob]) => {
-        setRecURL(URL.createObjectURL(blob))
-        setIsRecording(false)
-      })
-      .catch((e: Error) => console.log(e))
-  }
-
   return (
     <>
       {/* BG */}
@@ -118,40 +105,12 @@ export default function Home() {
             ))}
           </div>
           {/* TODO: add ability to answer host questions */}
-          {/* {recURL && <audio src={recURL} controls />} */}
           {/* COMMENTS */}
           <div className="flex w-full flex-col items-center space-y-4">
             {/* HEADING */}
             <h2 className="text-xl font-bold dark:text-white">Comments</h2>
             {/* BOX */}
-            <div>
-              <button
-                onClick={() =>
-                  isRecording ? stopRecording() : startRecording()
-                }
-                className={`flex w-full appearance-none flex-row items-center justify-center space-x-1 rounded-t-md py-2 px-3 text-zinc-100 outline-offset-2 transition duration-200 active:text-zinc-100/80 active:transition-none dark:active:text-zinc-100/70 ${
-                  isRecording
-                    ? 'bg-red-500 hover:bg-red-400 active:bg-red-500 dark:bg-red-700 dark:hover:bg-red-600 dark:active:bg-red-700'
-                    : 'bg-green-600 hover:bg-green-500 active:bg-green-600 dark:bg-green-700 dark:hover:bg-green-600 dark:active:bg-green-700'
-                }`}
-              >
-                <div className="text-lg">
-                  {isRecording ? <FaStopCircle /> : <FaMicrophone />}
-                </div>
-                <p className="text-sm font-semibold">
-                  {isRecording ? 'Stop' : 'Record'}
-                </p>
-              </button>
-              {/* TEXT BOX */}
-              <TextareaAutosize
-                className="w-full resize-none appearance-none border-x border-zinc-900/10 bg-white px-3 py-2 shadow-md shadow-zinc-800/5 transition-colors placeholder:text-zinc-400 focus:outline-none dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500"
-                placeholder="Comment..."
-                minRows={3}
-              />
-              <button className="-mt-1 w-full rounded-b-md bg-zinc-500 py-2 px-3 text-sm font-semibold text-zinc-100 outline-offset-2 transition hover:bg-zinc-400 active:bg-zinc-500 active:text-zinc-100/80 active:transition-none dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70">
-                Comment
-              </button>
-            </div>
+            <CommentBox />
             {/* COMMENTS */}
             <div className="flex w-full flex-col space-y-4">
               {podcast.comments.map((comment) => (
@@ -174,6 +133,129 @@ export default function Home() {
         </div>
       </div>
     </>
+  )
+}
+
+const Mp3Recorder = new MicRecorder({ bitRate: 128 })
+const CommentBox = () => {
+  const [isRecording, setIsRecording] = useState(false)
+  const [recURL, setRecURL] = useState<string | null>(null)
+
+  const startRecording = async () => {
+    setIsRecording(true)
+    Mp3Recorder.start()
+      .then(() => {})
+      .catch((e: Error) => console.error(e))
+  }
+  const stopRecording = async () => {
+    Mp3Recorder.stop()
+      .getMp3()
+      .then(([_, blob]: [any, Blob]) => {
+        setRecURL(URL.createObjectURL(blob))
+        setIsRecording(false)
+      })
+      .catch((e: Error) => console.log(e))
+  }
+  return (
+    <div className="w-full">
+      {/* {recURL && <audio src={recURL} controls />} */}
+      {/* RECORD / STOP */}
+      <button
+        onClick={() => (isRecording ? stopRecording() : startRecording())}
+        className={`flex w-full appearance-none flex-row items-center justify-center space-x-1 rounded-t-md py-2 px-3 text-zinc-100 outline-offset-2 transition duration-200 active:text-zinc-100/80 active:transition-none dark:active:text-zinc-100/70 ${
+          isRecording
+            ? 'bg-red-500 hover:bg-red-400 active:bg-red-500 dark:bg-red-700 dark:hover:bg-red-600 dark:active:bg-red-700'
+            : 'bg-green-600 hover:bg-green-500 active:bg-green-600 dark:bg-green-700 dark:hover:bg-green-600 dark:active:bg-green-700'
+        }`}
+      >
+        <div className="text-lg">
+          {isRecording ? <FaStopCircle /> : <FaMicrophone />}
+        </div>
+        <p className="text-sm font-semibold">
+          {isRecording ? 'Stop' : 'Record'}
+        </p>
+      </button>
+      {/* TEXT BOX */}
+      {recURL ? (
+        <VoiceWave src={recURL || ''} trash={() => setRecURL(null)} />
+      ) : (
+        <TextareaAutosize
+          className="w-full resize-none appearance-none border-x border-zinc-900/10 bg-white px-3 py-2 shadow-md shadow-zinc-800/5 transition-colors placeholder:text-zinc-400 focus:outline-none dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500"
+          placeholder="Comment..."
+          minRows={3}
+        />
+      )}
+      <button className="-mt-1 w-full rounded-b-md bg-zinc-500 py-2 px-3 text-sm font-semibold text-zinc-100 outline-offset-2 transition hover:bg-zinc-400 active:bg-zinc-500 active:text-zinc-100/80 active:transition-none dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70">
+        Comment
+      </button>
+    </div>
+  )
+}
+
+const VoiceWave = ({ src, trash }: { src: string; trash: () => void }) => {
+  const [wavesurfer, setWavesurfer] = useState<WaveSurfer | null>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const hasMounted = useRef(false)
+
+  useEffect(() => {
+    if (hasMounted.current == false) {
+      ;(async () => {
+        const WaveSurfer = (await import('wavesurfer.js')).default
+
+        let tWavesurfer = WaveSurfer.create({
+          container: '#waveform',
+          waveColor: '#a1a1aa',
+          progressColor: '#e4e4e7',
+          barWidth: 4,
+          barRadius: 4,
+          cursorWidth: 1,
+          height: 50,
+          barGap: 3,
+          barMinHeight: 5,
+          cursorColor: 'transparent',
+          normalize: true,
+          interact: false,
+        })
+        tWavesurfer.on('finish', () => setIsPlaying(false))
+        tWavesurfer.on('play', () => setIsPlaying(true))
+        tWavesurfer.on('pause', () => setIsPlaying(false))
+        tWavesurfer.on('ready', () => setIsPlaying(false))
+        setWavesurfer(tWavesurfer)
+      })()
+
+      return () => {
+        hasMounted.current = true
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (wavesurfer && src) {
+      // wavesurfer.empty()
+      wavesurfer.load(src)
+    }
+  }, [wavesurfer, src])
+
+  return (
+    <div className="flex w-full appearance-none flex-row items-center space-x-4 border-x border-zinc-900/10 bg-white p-3 shadow-md shadow-zinc-800/5 transition-colors placeholder:text-zinc-400 focus:outline-none dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500">
+      <button
+        className="p-1 dark:text-zinc-200 dark:hover:text-zinc-300 dark:active:text-zinc-200"
+        onClick={() => wavesurfer?.playPause()}
+        disabled={!src}
+      >
+        {isPlaying ? <FaPause /> : <FaPlay />}
+      </button>
+      <div id="waveform" className="flex-1"></div>
+      <button
+        className="p-1 dark:text-zinc-200 dark:hover:text-zinc-300 dark:active:text-zinc-200"
+        onClick={() => {
+          trash()
+          wavesurfer?.empty()
+        }}
+      >
+        <FaTrash />
+      </button>
+    </div>
   )
 }
 
