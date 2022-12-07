d4d942de0292a7c7872231dc61139edfe21ae71b
Folded-Code <thomas.reilly.forbes@gmail.com>
2022-12-07 11:45:25 +0000

    slight ui tweaks

--- src/components/episode/CommentBox.tsx
+++ src/components/episode/CommentBox.tsx
@@ -2,7 +2,7 @@ import { Comment, User } from '@prisma/client'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import { useContext, useState } from 'react'
-import { FaHeart, FaRegHeart } from 'react-icons/fa'
+import { FaHeart, FaRegHeart, FaUserCircle } from 'react-icons/fa'
 import { DataContext } from '../../pages/[showSlug]/[episodeSlug]'
 import { trpc } from '../../utils/trpc'
 
@@ -40,7 +40,8 @@ export default function CommentBox({
             width={20}
           />
         ) : (
-          <div className="h-5 w-5 rounded-full bg-black dark:bg-white" />
+          // <div className="h-5 w-5 rounded-full bg-black dark:bg-white" />
+          <FaUserCircle className="text-xl" />
         )}
         <div className="flex flex-col space-y-1">
           <p className="text-sm font-bold">{comment.user.name ?? 'No name'}</p>
@@ -49,7 +50,7 @@ export default function CommentBox({
         </div>
       </div>
       {/* BOTTOM ACTIONS */}
-      <div className="flex flex-row items-center space-x-2 text-zinc-500/80 dark:text-zinc-400">
+      <div className="ml-[2px] flex flex-row items-center space-x-2 text-zinc-500/80 dark:text-zinc-400">
         <button
           className="appearance-none py-1 text-sm"
           onClick={() => addLike.mutate({ commentId: comment.id })}
